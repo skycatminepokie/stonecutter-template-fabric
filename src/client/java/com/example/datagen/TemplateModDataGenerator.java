f2feac8ca08989db5c4745544ef48ce3c04a44b2
skycatminepokie <skycat@skycatdev.com>
2025-11-05 21:30:15 -0800

    Fix import in datagen

--- src/client/java/com/example/datagen/TemplateModDataGenerator.java
+++ src/client/java/com/example/datagen/TemplateModDataGenerator.java
@@ -1,6 +1,7 @@
 package com.example.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class TemplateModDataGenerator implements DataGeneratorEntrypoint {
     @Override
